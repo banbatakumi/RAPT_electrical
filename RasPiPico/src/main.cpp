f76adf7cc636c131af32d83253380466476a003c
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-02-25 15:57:48 +0900

    UART受信できた

--- RasPiPico/src/main.cpp
+++ RasPiPico/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
+      Serial.begin(9600);
+      Serial1.begin(9600);  // UART0初期化 TX:GP0 / RX:GP1
 }
 
 void loop() {
-      Serial.println("Hello World");
+      if (Serial1.available()) {
+            Serial.println("received data");
+            Serial.print(Serial1.read());
+      }
       sleep_ms(100);
 }
